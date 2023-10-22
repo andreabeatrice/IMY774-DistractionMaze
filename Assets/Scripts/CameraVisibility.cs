219ad76884ac99f64081d2702cb9f75b971dbbc0
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2023-10-22 14:46:40 +0200

    IsVisible + Distance Grab for books

---
+++ Assets/Scripts/CameraVisibility.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVisibility : MonoBehaviour
+{
+    public GameObject target;
+    public Camera cam;
+
+    public GameObject RedBook, OrangeBook, OrangeYellowBook, YellowBook, GreenBook, GreenBlueBook, BlueBook, PurpleBook, PinkPurpleBook, PinkBook;
+
+    private bool IsVisible(Camera c, GameObject target)
+    {
+        var planes = GeometryUtility.CalculateFrustumPlanes(c);
+        var point = target.transform.position;
+
+        foreach (var plane in planes)
+        {
+            if (plane.GetDistanceToPoint(point)< 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void Update ()
+    {
+        
+
+        var targetRender = target.GetComponent<Renderer>();
+        if (IsVisible(cam,target))
+        {
+           
+        }
+        else
+        {
+           Debug.Log("Not looking at teable!!!!!!!!!!!!!!");
+
+           if (RedBook.activeSelf){
+                RedBook.transform.position = new Vector3(-2.4411f, 1.116f, 1.003f);
+                RedBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                OrangeBook.transform.position = new Vector3(-2.3384f, 1.142f, 1.029498f);
+                OrangeBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                OrangeYellowBook.transform.position = new Vector3(-2.496f, 1.126f, 1.003f);
+                OrangeYellowBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                YellowBook.transform.position = new Vector3(-2.2887f, 1.103f, 1.003f);
+                YellowBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                GreenBook.transform.position = new Vector3(-2.6075f, 1.116f, 0.9853f);
+                GreenBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                GreenBlueBook.transform.position = new Vector3(-2.3894f, 1.107f, 0.9732f);
+                GreenBlueBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                BlueBook.transform.position = new Vector3(-2.1877f, 1.138f, 1.003f);
+                BlueBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                PurpleBook.transform.position = new Vector3(-2.242f, 1.154f, 1.003f);
+                PurpleBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                PinkPurpleBook.transform.position = new Vector3(-2.6688f, 1.128f, 0.9954979f);
+                PinkPurpleBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+                PinkBook.transform.position = new Vector3(-2.5536f, 1.147f, 1.003f);
+                PinkBook.transform.eulerAngles = new Vector3(-90, 0, 90);
+            }
+        }
+    }
+}
+
